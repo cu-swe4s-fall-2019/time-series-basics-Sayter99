9a3343505456c713cdaccd615c7e9fc03f47f966
Sayter99 <ing5698yi@hotmail.com>
2019-10-08 22:32:51 -0600

    improve infrastructure

--- data_import.py
+++ data_import.py
@@ -21,13 +21,49 @@ class ImportData:
     def linear_search_value(self, key_time):
         # return list of value(s) associated with key_time
         # if none, return -1 and error message
-        pass
+        result = []
+        for i in range(len(self._time)):
+            if self._time[i] == key_time:
+                result.append(self._value[i])
+        if (len(result) == 0):
+            print('Cannot find any value associated with key_time')
+            return -1
+        return result
 
     def binary_search_value(self, key_time):
         # optional extra credit
         # return list of value(s) associated with key_time
         # if none, return -1 and error message
-        pass
+        result = []
+        low = 0
+        high = len(self._time) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if (self._time[mid] == key_time):
+                result.append(self._value[mid])
+                left = mid - 1
+                while left > 0:
+                    if self._time[left] == key_time:
+                        result.append(self._value[left])
+                        left = left - 1
+                    else:
+                        break
+                right = mid + 1
+                while right < len(self._time):
+                    if self._time[right] == key_time:
+                        result.append(self._value[right])
+                        right = right + 1
+                    else:
+                        break
+            elif (self._time[mid] < key_time):
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        if (len(result) == 0):
+            print('Cannot find any value associated with key_time')
+            return -1
+        return result
 
 
 def roundTimeArray(obj, res):
