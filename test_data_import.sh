45124f92374b4b9396b3da8dda2665d3a404a65a
Sayter99 <ing5698yi@hotmail.com>
2019-10-09 17:30:55 -0600

    complete print function

--- test_data_import.sh
+++ test_data_import.sh
@@ -7,3 +7,17 @@ run test_style pycodestyle data_import.py
 assert_no_stdout
 run test_style pycodestyle test_data_import.py
 assert_no_stdout
+
+run bad_sort_key python data_import.py --folder_name smallData --output_file aaa --sort_key aa
+assert_stdout
+assert_exit_code 1
+
+run bad_folder python data_import.py --folder_name ddd --output_file aaa --sort_key aa
+assert_stdout
+assert_exit_code 1
+
+run basic_test python data_import.py --folder_name smallData --output_file aaa --sort_key hr_small.csv
+assert_stdout
+assert_exit_code 0
+rm aaa_5.csv
+rm aaa_15.csv
